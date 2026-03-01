58024cb84daa9b368cb9e89e11afabad51d57e81
USHASRI1112 <ushasrigudikandula456@gmail.com>
2026-03-01 19:01:17 +0530

    feat: app setup

--- App.tsx
+++ App.tsx
@@ -1,45 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
+import { shouldRefetchMatches } from './src/helpers/ShouldRefetchMatches';
+import AllMatchesScreen from './src/screens/AllMatchesScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import MatchScreen from './src/screens/MatchScreen';
+import { fetchMatches } from './src/services/Matches';
+import { Match } from './src/types';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+export type RootStackParamList = {
+  Home: undefined;
+  AllMatches: undefined;
+  Match: { matchId: string; match: Match };
+};
 
-function App() {
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const queryClient = new QueryClient();
+
+export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  useEffect(() => {
+    const checkRefetch = async () => {
+      try {
+        const should = await shouldRefetchMatches();
+        if (should) {
+          const data = await fetchMatches();
+          queryClient.setQueryData(['ALL_MATCHES', 'storage'], data);
+        }
+      } catch (e) {
+        console.warn('Error during startup refetch check', e);
+      }
+    };
+
+    checkRefetch();
+  }, []);
+
   return (
-    <SafeAreaProvider>
+    <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: isDarkMode ? '#1f2937' : '#ffffff',
+              },
+              headerTintColor: isDarkMode ? '#ffffff' : '#000000',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          >
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                // title: '🏏 Cricket Prediction',
+                // headerStyle: {
+                //   backgroundColor: isDarkMode ? '#1f2937' : '#3b82f6',
+                // },
+                headerShown: false,
+                headerTintColor: '#ffffff',
+              }}
+            />
+            <Stack.Screen
+              name="AllMatches"
+              component={AllMatchesScreen}
+              options={{
+                title: 'All Matches',
+              }}
+            />
+            <Stack.Screen
+              name="Match"
+              component={MatchScreen}
+              options={{
+                title: 'Match Prediction',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
+    </>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
